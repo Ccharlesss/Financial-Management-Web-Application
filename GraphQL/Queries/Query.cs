6c3c02437cc2f0cede1d41c8d2d0a2c74aa47dde
Ccharlesss <romain.kuhne@bluewin.ch>
2025-02-16 14:14:51 +0100

    list users added to query.cs

--- GraphQL/Queries/Query.cs
+++ GraphQL/Queries/Query.cs
@@ -1,8 +1,64 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
 using ManageFinance.Models;
-using HotChocolate;
-
+using ManageFinance.Services;
 public class Query
 {
+  private readonly UserManager<AppUser> _userManager;
+
+  private readonly RoleManager<AppUser> _roleManager;
+
+  private readonly IConfiguration _configuration;
+
+  private readonly ILogger<Query> _logger;
+
+
+public Query(
+  UserManager<AppUser> userManager,
+  RoleManager<AppUser> roleManager,
+  IConfiguration configuration,
+  ILogger<Query> logger)
+  
+  {
+    _userManager = userManager;
+    _roleManager = roleManager;
+    _configuration = configuration;;
+    _logger = logger;
+  }
+
+
+  //=============================================================================================================================
+  // Purpose: Handles the logic for listing all users:
+  public async Task<List<object>> GetAllUsers()
+  { // 1) Fetch all users from the DB:
+    var users = await _userManager.Users.ToListAsync();
+    // 2) Define a new list that will store users details:
+    var userList = new List<object>();
+    // 3) Iterate over all users in the users list:
+    foreach(var user in users)
+    { // 3.1) Fetch the role of the user:
+      var roles = await _userManager.GetRolesAsync(user);
+      userList.Add(new
+      {
+        user.Id,
+        user.UserName,
+        user.Email,
+        roles,
+      });
+    }
+
+    return userList;
+
+  }
+
+
+
+
+
+
+
+
   
 }
