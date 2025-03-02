70b28df8a2f34da5d0ac2b8d017eb10e48e87cd4
Ccharlesss <romain.kuhne@bluewin.ch>
2025-03-02 11:57:00 +0100

    Unit test cases done for Register method in AccountController.cs

--- ManageFinance/obj/Debug/net8.0/ManageFinance.AssemblyInfo.cs
+++ ManageFinance/obj/Debug/net8.0/ManageFinance.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ManageFinance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0821bf89eb9abf704b6a9b4b1c5689e29e35b981")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e892c692b8ab3b0eea6446a16beb0bfcbc856730")]
 [assembly: System.Reflection.AssemblyProductAttribute("ManageFinance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ManageFinance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
